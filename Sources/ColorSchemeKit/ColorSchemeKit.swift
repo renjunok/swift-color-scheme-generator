2272e68bbacb32bb7f6dc30c843564aa322e1754
renjun <jamesbond.ren@gmail.com>
2024-06-12 23:08:41 +0800

    Releases 1.0.0

--- Sources/ColorSchemeKit/ColorSchemeKit.swift
+++ Sources/ColorSchemeKit/ColorSchemeKit.swift
@@ -1,5 +1,3 @@
-import CoreGraphics
-
 //
 //  ColorSchemeKit.swift
 //  swift-color-scheme-kit
@@ -8,83 +6,174 @@ import CoreGraphics
 //
 
 
-// MARK: scheme
-enum ColorScheme {
-    case complementary  // 对比，  180度
-    case monochromatic  // 单色调 // 只调节饱和度和明亮度
-    case analogous  // 近似色 // 60度内
-    case triadic  // 三分  // 120度
-    case tetradic  // 四分  // 90度  主色(蓝)60%  30%洋红/白(平均分配) , 10%亮绿色和黄色平均分配
+import SwiftUI
+
+
+@available(macOS 11.0, iOS 14, *)
+public class ColorSchemeKit: ObservableObject {
+    
+    public typealias ColorSet2 = (c1: Color?, c2: Color?)
+    public typealias ColorSet3 = (c1: Color?, c2: Color?, c3: Color?)
+    public typealias Secondary = (monochromatic: ColorSet2,
+                                  analogous: ColorSet2,
+                                  triadic: ColorSet2,
+                                  tetradic: ColorSet3
+    )
+    
+    @Published private var primaryHSB: Color.HSB?
+    
+    // MARK: primary color
+    public var primary: Color? {
+        guard let hsb = primaryHSB else { return nil }
+        return Color(hue: hsb.hue, saturation: hsb.saturation, brightness: hsb.brightness)
+    }
+    
+    // MARK: accent color
+    public var accent: Color? {
+        adjustedHue(by: .degrees(180))
+    }
+    
+    // MARK: shadown color
+    public var shadow: Color? {
+        adjustedHue(by: .degrees(180))
+    }
+    
+    // MARK:
+    public var secondary: Secondary {
+        (monochromatic: monochromaticSet, analogous: analogousSet, triadic: triadicSet, tetradic: tetradicSet)
+    }
+    
+    // MARK: monochromatic Set
+    // Change saturation and brightness
+    public var monochromaticSet: ColorSet2 {
+        (adjustedSaturation(by: -0.5), adjustedBrightness(by: -0.5))
+    }
+    
+    // MARK: analogousSet
+    // Adjacent 60 degrees
+    public var analogousSet: ColorSet2 {
+        (adjustedHue(by: .degrees(60)), adjustedHue(by: .degrees(-60)))
+    }
+    
+    // MARK: triadicSet
+    // 120 degress
+    public var triadicSet: ColorSet2 {
+        (c1: adjustedHue(by: .degrees(120)), c2: adjustedHue(by: .degrees(-120)))
+    }
+    
+    // MARK: tetradicSet
+    // 90 degress
+    public var tetradicSet: ColorSet3 {
+        (c1: adjustedHue(by: .degrees(90)), c2: adjustedHue(by: .degrees(180)), c3: adjustedHue(by: .degrees(270)))
+    }
+    
+    // MARK: neutral color
+    // Make the primary color more prominent, black, white, gray, brown, as the background, with a sense of space, breathing, and white space
+    public var neutral: ColorSet3 {
+        (c1: .black, c2: .white, c3: .gray)
+    }
+    
+    
+    // Color init
+    public init(color: Color) {
+        self.primaryHSB = color.getHSB()
+    }
+    
+    // Hexadecimal initialization
+    public init(hex: Int) {
+        self.primaryHSB = Color(hex: hex).getHSB()
+    }
+    
+    
+}
+
+// MARK: primary color related functions
+@available(macOS 11.0, iOS 14, *)
+extension ColorSchemeKit {
+    
+    /// Adjust primary color hue
+    /// - Parameters:
+    ///   - hue: An angle value,
+    ///
+    /// Adjust primary color hue
+    public func adjustedPrimary(hue angle: Angle) {
+        guard let hsb = primaryHSB else { return }
+        let newNormalizeHue =  normalizeAngle((hsb.hue * 360) + angle.degrees) / 360
+        primaryHSB?.hue = newNormalizeHue
+    }
+    
+    /// Adjust primary color saturation
+    /// - Parameters:
+    ///   - saturation: 0 ~ 1,  Positive numbers increase, negative numbers decrease
+    ///
+    /// Adjust primary color saturation
+    public func adjustedPrimary(saturation amount: Double) {
+        guard let hsb = primaryHSB else { return }
+        primaryHSB?.saturation = min(max(hsb.saturation + amount, 0), 1)
+    }
+    
+    /// Adjust primary color brightness
+    /// - Parameters:
+    ///   - brightness: 0 ~ 1,  Positive numbers increase, negative numbers decrease
+    ///
+    /// Adjust primary color brightness
+    // 调整主色明度
+    public func adjustedPrimary(brightness amount: Double) {
+        guard let hsb = primaryHSB else { return }
+        primaryHSB?.brightness = min(max(hsb.brightness + amount, 0), 1)
+    }
+    
+    ///  Modify the primary color
+    /// - Parameters:
+    ///   - hue: An angle value,
+    ///   - saturation: 0 ~ 1,  Positive numbers increase, negative numbers decrease
+    ///   - brightness: 0 ~ 1,  Positive numbers increase, negative numbers decrease
+    ///
+    /// Modify the primary color
+    public func modifyPrimary(hue angle: Angle, saturation amount1: Double, brightness amount2: Double) {
+        guard primaryHSB != nil else { return }
+        
+        // /360 Make sure the new hue value is between 0 and 1
+        let newNormalizeHue = normalizeAngle(angle.degrees) / 360
+        
+        primaryHSB?.hue = newNormalizeHue
+        primaryHSB?.saturation = min(max(amount1, 0), 1)
+        primaryHSB?.brightness = min(max(amount2, 0), 1)
+    }
+    
 }
 
 
-public class ColorSchemeKit {
-    private(set) var primary: String
+// MARK:  library related functions
+@available(macOS 11.0, iOS 14, *)
+private extension ColorSchemeKit {
     
-    var complementary: String {
-        primary + "comp"
+    // MARK: Adjust hue
+    private func adjustedHue(by angle: Angle) -> Color? {
+        guard let hsb = primaryHSB else { return nil }
+        let newNormalizeHue =  normalizeAngle((hsb.hue * 360) + angle.degrees) / 360
+        return Color(hue: newNormalizeHue, saturation: hsb.saturation, brightness: hsb.brightness)
     }
     
-    // 单色调, 三个
-    var secondary: String { primary + "1"}
-    var secondary2: String? { primary + "2" }
-    var secondary3: String? { primary + "3" }
-    
-    // MARK: accent color, 最多2个
-    var accent: String {
-        primary + " accent"
-    }
-    var accent2: String? {
-        primary + " accent"
+    // MARK: Adjust saturation
+    private func adjustedSaturation(by amount: Double) -> Color? {
+        guard let hsb = primaryHSB else { return nil }
+        
+        let newSaturation =  min(max(hsb.saturation + amount, 0), 1)
+        return Color(hue: hsb.hue, saturation: newSaturation, brightness: hsb.brightness)
     }
     
-    // 阴影 , 在主色调中添加黑色
-    var shade: String {
-        primary + "shade"
+    // MARK: Adjust brightness
+    private func adjustedBrightness(by amount: Double) -> Color? {
+        guard let hsb = primaryHSB else { return nil }
+        let newBrightness = min(max(hsb.brightness + amount, 0), 1)
+        return Color(hue: hsb.hue, saturation: hsb.saturation, brightness: newBrightness)
     }
     
-    var dark: String {
-        primary + "dark"
+    // MARK: Normalize angle
+    private func normalizeAngle(_ angle: Double) -> Double {
+        let normalizedAngle = angle.truncatingRemainder(dividingBy: 360)
+        return normalizedAngle < 0 ? normalizedAngle + 360 : normalizedAngle
     }
-    
-    // 中性色，让主色更突出，是黑色，白色，灰，棕色，做为背景，有空间感，呼吸感，留白
-    var neutral: String { "1" }
-    var neutral2: String { "1" }
-    var neutral3: String { "1" }
-    var neutral4: String { "1" }
-    var neutral5: String { "1" }
-    
-    // 调整主要色
-    func changePrimary(_ value: CGFloat) {
-        self.primary = self.primary + "\(value)"
-    }
-    
-    
-    // 微调
-    // 次要色调整，但要限定在度数内0-1, 默认0.5(数字检测)
-    
-    public init(primaryColor: String) {
-        self.primary = primaryColor
-    }
-    
-//    static let shared = ColorSchemeKit()
-    
-    // 单例
-    // 16进制初始化(数值检测)
-    // hsv初始化
-    // 系统颜色初始化
-    // 初始化需要传入配色方案吗？
-    
-    // 1 16进制转hsv
-    //
-    
-    
-    
-    // 生成次要色(单色、多色几种方案)
-    
-    
-    
-    
     
 }
-
--- Tests/ColorSchemeKitTests/ColorSchemeKitTests.swift
+++ Tests/ColorSchemeKitTests/ColorSchemeKitTests.swift
@@ -1,15 +1,18 @@
 import XCTest
 @testable import ColorSchemeKit
 
+@available(macOS 11.0, iOS 14, *)
 final class ColorSchemeKitTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-//        XCTAssertEqual(ColorSchemeKit().text, "Hello, World!")
+        let hexColor = 0xFF0000 // Example hex color value
+        let colorScheme = ColorSchemeKit(hex: hexColor)
         
-        let a = ColorSchemeKit()
-//        a.secondary = "sdf"
-        print(a.secondary)
+        // results.
+        XCTAssertEqual(colorScheme.neutral.c1!, .black)
+        
+        // TODO: add test case...
+        
     }
 }
